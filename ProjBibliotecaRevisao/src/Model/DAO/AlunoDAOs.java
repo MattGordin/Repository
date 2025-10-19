2061697fc8f5587e23ea066eab555200a6301a10
Matteus <matteusgordin1199@gmail.com>
2025-10-19 11:13:51 -0300

    test

---
+++ ProjBibliotecaRevisao/src/Model/DAO/AlunoDAOs.java
@@ -0,0 +1,9 @@
+package Model.DAO;
+
+import Model.Entidade.Aluno;
+
+public class AlunoDAOs {
+    public void cadastrarAluno(Aluno aluno){
+        String sql = "insert into pessoa (aluno, matricula, curso, periodo, statusMatricula) values ("+ aluno.getAluno() + "," + aluno.getMatricula() + "," + aluno.getCurso() + "," + aluno.getPeriodo() + "," + aluno.getStatusMatricula() +")";
+        }
+}
---
+++ ProjBibliotecaRevisao/src/Model/DAO/EmprestimoDAOs.java
@@ -0,0 +1,9 @@
+package Model.DAO;
+
+import Model.Entidade.Pessoa;
+
+public class EmprestimoDAOs {
+    public void cadastrarPessoa(Pessoa pessoa){
+        String sql = "insert into pessoa (nome, cpf, telefone, endereco, email, dataNascimento, sexo) values ("+ pessoa.getNome() + "," + pessoa.getCpf() + "," + pessoa.getTelefone() + "," + pessoa.getEndereco() + "," + pessoa.getEmail() + "," + pessoa.getDataNascimento() + "," + pessoa.getSexo() +")";
+    }
+}
